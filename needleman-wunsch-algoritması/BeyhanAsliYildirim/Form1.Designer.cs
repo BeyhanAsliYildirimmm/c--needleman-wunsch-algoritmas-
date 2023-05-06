9620dedf14ec9daf9de790753dd5be701a6240a9
Beyhan Aslı <yildirimbeyhan6@gmail.com>
2023-05-06 15:53:57 +0300

    needleman wusch algitması
    c# ile needleman wusch algitmasının bir kısmı geliştirildi

---
+++ needleman-wunsch-algoritması/BeyhanAsliYildirim/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿using System;
+
+namespace alignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.matchValue = new System.Windows.Forms.TextBox();
+            this.match = new System.Windows.Forms.Label();
+            this.misMatch = new System.Windows.Forms.Label();
+            this.misMatchValue = new System.Windows.Forms.TextBox();
+            this.gap = new System.Windows.Forms.Label();
+            this.gapValue = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dna1 = new System.Windows.Forms.TextBox();
+            this.dna2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // matchValue
+            // 
+            this.matchValue.Location = new System.Drawing.Point(231, 24);
+            this.matchValue.Name = "matchValue";
+            this.matchValue.Size = new System.Drawing.Size(100, 22);
+            this.matchValue.TabIndex = 0;
+            // 
+            // match
+            // 
+            this.match.AutoSize = true;
+            this.match.Location = new System.Drawing.Point(82, 28);
+            this.match.Name = "match";
+            this.match.Size = new System.Drawing.Size(43, 16);
+            this.match.TabIndex = 1;
+            this.match.Text = "match";
+            // 
+            // misMatch
+            // 
+            this.misMatch.AutoSize = true;
+            this.misMatch.Location = new System.Drawing.Point(82, 74);
+            this.misMatch.Name = "misMatch";
+            this.misMatch.Size = new System.Drawing.Size(64, 16);
+            this.misMatch.TabIndex = 3;
+            this.misMatch.Text = "misMatch";
+            // 
+            // misMatchValue
+            // 
+            this.misMatchValue.Location = new System.Drawing.Point(231, 70);
+            this.misMatchValue.Name = "misMatchValue";
+            this.misMatchValue.Size = new System.Drawing.Size(100, 22);
+            this.misMatchValue.TabIndex = 2;
+            // 
+            // gap
+            // 
+            this.gap.AutoSize = true;
+            this.gap.Location = new System.Drawing.Point(82, 122);
+            this.gap.Name = "gap";
+            this.gap.Size = new System.Drawing.Size(31, 16);
+            this.gap.TabIndex = 5;
+            this.gap.Text = "gap";
+            // 
+            // gapValue
+            // 
+            this.gapValue.Location = new System.Drawing.Point(231, 118);
+            this.gapValue.Name = "gapValue";
+            this.gapValue.Size = new System.Drawing.Size(100, 22);
+            this.gapValue.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(875, 581);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 47);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Çalıştır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(512, 122);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(745, 433);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 581);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Çalışma Zamanı:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 581);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(482, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "DNA1:";
+            // 
+            // dna1
+            // 
+            this.dna1.Location = new System.Drawing.Point(545, 25);
+            this.dna1.Name = "dna1";
+            this.dna1.Size = new System.Drawing.Size(248, 22);
+            this.dna1.TabIndex = 12;
+            // 
+            // dna2
+            // 
+            this.dna2.Location = new System.Drawing.Point(890, 24);
+            this.dna2.Name = "dna2";
+            this.dna2.Size = new System.Drawing.Size(237, 22);
+            this.dna2.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(841, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "DNA2:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(62, 177);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(376, 324);
+            this.listBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1269, 657);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dna2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dna1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gap);
+            this.Controls.Add(this.gapValue);
+            this.Controls.Add(this.misMatch);
+            this.Controls.Add(this.misMatchValue);
+            this.Controls.Add(this.match);
+            this.Controls.Add(this.matchValue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox matchValue;
+        private System.Windows.Forms.Label match;
+        private System.Windows.Forms.Label misMatch;
+        private System.Windows.Forms.TextBox misMatchValue;
+        private System.Windows.Forms.Label gap;
+        private System.Windows.Forms.TextBox gapValue;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox dna1;
+        private System.Windows.Forms.TextBox dna2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ needleman-wunsch-algoritması/BeyhanAsliYildirim/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System.Security.Cryptography;
+using System.IO;
+using System.Drawing;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace alignment
+{
+    using System;
+    using System.Diagnostics; //Çalışma zamanını hesaplamak için kullanıyoruz.
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        string filePath1 = "";
+        string path = "";
+        char[] path1Chars = null;
+        char[] path2Chars = null;
+
+        int[,] values = new int[6, 6];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string fileLoad()
+        {
+            // OpenFileDialog ile dosyayı yüklemezsiniz, sadece dosya ile ilgili bilgileri almış olursunuz.
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            //dosya seçimi için başlanğıç noktası seçiyoruz.( InitialDirectory özelliği sayesinde)
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //Environment.GetFolderPath(Environment.SpecialFolder.Desktop) yazılımı bize bilgisayardaki Masaüstü klasör ismini gönderen bir yöntemdir.
+
+            dialog.Filter = "txt Dosyası | *.txt";
+            //klasörler için filtreleme yapmaktır. Bu işlem için de seçilmesini istediğiniz dosya formatlarını virgülle ayırarak eklediğimiz yerdir.
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                //FileName özelliğin dosya yolunu gösterdiğidir
+                filePath1 = dialog.FileName;
+                return filePath1;
+            }
+            else
+            {
+                return "Dosya Seçilmedi";
+            }
+        }
+        public string fileRead(string filePath)
+        {
+
+            // StreamReader sınıfı dosya okuma için kullanılır
+            StreamReader sr = new StreamReader(filePath);
+            //metni tek seferde baştan sona kadar okur.
+            path = sr.ReadToEnd();
+            return path;
+
+        }
+        public int oneCalculate(int i, int j, int match, int misMatch)
+        {
+            int deger = 0;
+            int sayi1 = i - 1;
+            int sayi2 = j - 1;
+            int sonuc = 0;
+            for (int n = 0; n < path1Chars.Length; n++)
+            {
+                for (int m = 0; m < path2Chars.Length; m++)
+                {
+                    if (path1Chars[i-1] == path2Chars[j-1])
+                    {
+                        for (int l = 0; l < values.Length; l++)
+                        {
+                            for (int k = 0; k < values.Length; k++)
+                            {
+                                if (l == sayi1 && k == sayi2)
+                                {
+                                    deger = values[sayi1, sayi2];
+                                    break;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+
+                        }
+                        sonuc = deger + match;
+                        break;
+
+                    }
+                    else
+                    {
+                        for (int l = 0; l < values.Length; l++)
+                        {
+                            for (int k = 0; k < values.Length; k++)
+                            {
+                                if (l == sayi1 && k == sayi2)
+                                {
+                                    deger = values[sayi1, sayi2];
+                                    break;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+
+                            }
+
+                        }
+                        sonuc = deger + misMatch;
+                        break;
+                    }
+
+                }
+
+
+            }
+            return sonuc;
+        }
+
+        public int twoCalculate(int i, int j, int gap)
+        {
+            int deger = 0;
+            int sayi = i - 1;
+            for (int l = 0; l < values.Length; l++)
+            {
+                for (int k = 0; k < values.Length; k++)
+                {
+                    if (l == sayi && j == k)
+                    {
+                        deger = values[sayi, j];
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+            }
+            int sonuc = deger + gap;
+            return sonuc;
+        }
+
+        public int threeCalculate(int i, int j, int gap)
+        {
+            int deger = 0;
+            int sayi = j - 1;
+
+            for (int l = 0; l < values.Length; l++)
+            {
+                for (int k = 0; k < values.Length; k++)
+                {
+                    if (l == i && k == sayi)
+                    {
+                        deger = values[i, sayi];
+                        break;
+                    }
+                }
+
+            }
+            int sonuc = deger + gap;
+
+            return sonuc;
+        }
+
+        public int bigNumber(int value1, int value2, int value3)
+        {
+            int number = 0;
+
+            if (value1 >= value2 && value1 >= value3)
+            {
+                return value1;
+
+            }
+            else if (value2 >= value1 && value2 >= value3)
+            {
+                return value2;
+            }
+            else if (value3 >= value2 && value3 >= value1)
+            {
+                return value3;
+            }
+            else
+            {
+                return number;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stopwatch watch = new Stopwatch(); // çalışma zamanı için Stopwatch nesnesini kullanıyoruz
+            watch.Start(); //başlatıyoruz.
+
+            int missMatch = -1, gap = -2, match = 1;
+            int path1Lenght = 0;
+            int path2Lenght = 0;
+
+            //missMatch, gap, match değerlerini boşsa değer atıyoruz değilse girilen değeri alıyoruz.
+            if (matchValue.Text == "")
+            {
+                match = 1;
+            }
+            else if (matchValue.Text != null)
+            {
+                match = Convert.ToInt32(misMatchValue.Text.ToString());
+            }
+            else if (misMatchValue.Text == "")
+            {
+                missMatch = -1;
+            }
+            else if (misMatchValue.Text != null)
+            {
+                missMatch = Convert.ToInt32(misMatchValue.Text.ToString());
+            }
+            else if (gapValue.Text == "")
+            {
+                gap = -2;
+            }
+            else if (gapValue.Text != null)
+            {
+                gap = Convert.ToInt32(gapValue.Text.ToString());
+            }
+
+            string path1; string path2; int sayac = 0;
+            string path1Read;
+            string path2Read;
+
+            //classdan nesene oluşturuyoruz.
+            Form1 file1 = new Form1();
+            //sayac 2 çünkü en fazla 2 dosya seçebiliriz.
+            while (sayac < 2)
+            {
+                if (sayac == 0)
+                {
+                    //ilk dosyamızı seçiyoruz.
+                    path1 = file1.fileLoad();
+                    //dosyamızın içini okuyoruz.
+                    path1Read = file1.fileRead(path1);
+                    //dosya içeriğini ekrana bastırıyoruz.
+                    dna1.Text = path1Read;
+                    //dosya uzunluğunu alıyoruz
+                    path1Lenght = path1Read.Length;
+                    //char dizisine atıyoruz
+                    path1Chars = path1Read.ToCharArray();
+                    sayac++;
+                }
+                else if (sayac == 1)
+                {
+                    //yurıdaki işlemleri 2 dosya içinde yapıyoruz.
+                    path2 = file1.fileLoad();
+                    path2Read = file1.fileRead(path2);
+                    //dosya içeriğini ekrana bastırıyoruz.
+                    dna2.Text = path2Read;
+                    path2Lenght = path2Read.Length;
+                    path2Chars = path2Read.ToCharArray();
+                    sayac++;
+                }
+
+            }
+            //tablo gorumunu için düzenleme yapıyoruz
+            dataGridView1.ColumnCount = 1 + path1Lenght;
+            dataGridView1.Columns[0].Name = " ";
+            dataGridView1.Columns[1].Name = " ";
+            //0.0 -> alanına 0 değerini koyuyoruz
+
+            int value1 = 0;
+            int value2 = 0;
+            int value3 = 0;
+            int answer = 0;
+            int answer2 = 0;
+            int answer3 = 0;
+
+
+            for (int i = 0; i <= path1Chars.Length; i++)
+            {
+                for (int k = 0; k <= path2Chars.Length; k++)
+                {
+                    if (i == 0 && k == 0)
+                    {
+                        values[i, k] = answer;
+                        
+
+                    }
+                    else if (i == 0 && k > 0)
+                    {
+                        //ilk satıra direkt gap degerini yerleştiriyoruz.
+                        answer += gap;
+                        dataGridView1.Rows.Add();
+                        dataGridView1.Rows[0].Cells[0].Value = "0";
+                        dataGridView1.Rows[i].Cells[k].Value = answer.ToString();
+                        values[i, k] = answer;
+                        
+
+
+                    }
+
+                    else if (i > 0 && k == 0)
+                    {
+                        //ilk sutuna direkt gap degerini yerleştiriyoruz.
+                        answer2 += gap;
+                        dataGridView1.Rows.Add();
+                        dataGridView1.Rows[i].Cells[k].Value = answer2.ToString();
+                        values[i, k] = answer2;
+                       
+                       
+                    }
+                }
+
+
+            }
+
+            for (int i = 1; i < path1Chars.Length+1; i++)
+            {
+                for (int k = 1; k < path2Chars.Length+1; k++)
+                {
+
+                    value1 = oneCalculate(i, k, match, missMatch);
+                    value2 = twoCalculate(i, k, gap);
+                    value3 = threeCalculate(i, k, gap);
+                    answer3 = bigNumber(value1, value2, value3);
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[i].Cells[k].Value = answer3.ToString();
+                    values[i,k] = answer3;
+                    listBox1.Items.Add(i);
+                    listBox1.Items.Add(k);
+                    listBox1.Items.Add(answer3);
+                                 
+                }
+
+        }
+
+
+            watch.Stop(); //durduruyoruz.
+                          //geçen süresi Elapsed değişkeni tutar.
+            textBox1.Text = watch.Elapsed.Seconds.ToString() + "sn";
+
+        
+    
+        }   
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        }
+            
+        }
+    
+
+
